bd8cbfeaeb23b8fdd540c1f16ba847c2457c3bba
George Desktop <georgecaws@gmail.com>
2021-08-28 18:49:22 +0100

    Prints albums by a given artist

--- source.py
+++ source.py
@@ -1,7 +1,28 @@
 # Load required libraries
-import json                 # For working with JSON data
+import json                                                 # For working with JSON data
+import spotipy                                              # Python wrapper for spotify API
+from spotipy.oauth2 import SpotifyClientCredentials         # Credential manager for spotify API
+import os                                                   # Used for setting environment variables
+
 
 # Load the api key
-with open("api-key.json") as file:
-    API_KEY = json.load(file)["api_key"]
 
+api_json = json.load(open("api-key.json"))
+
+os.environ["SPOTIPY_CLIENT_ID"] = api_json["client_id"]
+os.environ["SPOTIPY_CLIENT_SECRET"] = api_json["client_secret"]
+os.environ["SPOTIPY_REDIRECT_URI"] = api_json["redirect_uri"]
+
+
+tool_uri = 'spotify:artist:2yEwvVSSSUkcLeSTNyHKh8'
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+results = spotify.artist_albums(tool_uri, album_type='album')
+
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
