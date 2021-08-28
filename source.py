9115dae937a43edb924165d80d974b16fef460f0
GeorgeCaws <georgecaws@gmail.com>
2021-08-28 22:44:31 +0100

    Gets list of users unique artists

--- source.py
+++ source.py
@@ -1,9 +1,10 @@
 # Load required libraries
 import json                                                 # For working with JSON data
 import spotipy                                              # Python wrapper for spotify API
-from spotipy.oauth2 import SpotifyClientCredentials         # Credential manager for spotify API
+#from spotipy.oauth2 import SpotifyClientCredentials         # Credential manager for spotify API
+from spotipy.oauth2 import SpotifyOAuth
 import os                                                   # Used for setting environment variables
-
+import pandas as pd                                         # pandas for wrangling data
 
 # Load the api key
 
@@ -14,15 +15,42 @@ os.environ["SPOTIPY_CLIENT_SECRET"] = api_json["client_secret"]
 os.environ["SPOTIPY_REDIRECT_URI"] = api_json["redirect_uri"]
 
 
-tool_uri = 'spotify:artist:2yEwvVSSSUkcLeSTNyHKh8'
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+#tool_uri = 'spotify:artist:2yEwvVSSSUkcLeSTNyHKh8'
+#spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+#
+#results = spotify.artist_albums(tool_uri, album_type='album')
+#
+#albums = results['items']
+#while results['next']:
+#    results = spotify.next(results)
+#    albums.extend(results['items'])
+#
+#for album in albums:
+#    print(album["name"])
+#    print(album["release_date"])
 
-results = spotify.artist_albums(tool_uri, album_type='album')
 
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+scope = "user-library-read"
 
-for album in albums:
-    print(album['release_date'])
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+tracks_count = 500
+i = 0
+
+artist_list = pd.DataFrame(columns = ["artist"])
+
+while tracks_count != 50:
+    tracks = sp.current_user_saved_tracks(limit = 50, offset = i * 50)["items"]
+    tracks_count = len(tracks)
+    for j in range(0, tracks_count - 1):
+        for k in range(0, len(tracks[j]["track"]["artists"])):
+            artist = pd.DataFrame([tracks[j]["track"]["artists"][k]["uri"]], columns = ["artist"])
+            artist_list = artist_list.append(artist)
+    i += 1
+
+print(artist_list["artist"].unique())
+
+#results = sp.current_user_saved_tracks(limit = 50, offset = 0)
+#for idx, item in enumerate(results['items']):
+#    track = item['track']
+#    print(idx, track['artists'][0]['name'], " – ", track['name'])
