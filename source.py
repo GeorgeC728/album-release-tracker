454fdb411080e7958e0efe57b194c7a6147495ea
George Desktop <georgecaws@gmail.com>
2021-08-29 11:19:41 +0100

    Added comments and improved how the track loop

--- source.py
+++ source.py
@@ -1,18 +1,61 @@
+#### Perform required set up
+
 # Load required libraries
 import json                                                 # For working with JSON data
 import spotipy                                              # Python wrapper for spotify API
-#from spotipy.oauth2 import SpotifyClientCredentials         # Credential manager for spotify API
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials         # Allows access non-user data for spotify API
+from spotipy.oauth2 import SpotifyOAuth                     # Allows acessing user data for spotify API
 import os                                                   # Used for setting environment variables
-import pandas as pd                                         # pandas for wrangling data
+import pandas as pd                                         # For wrangling data
 
 # Load the api key
-
 api_json = json.load(open("api-key.json"))
 
+# Set environment variables as required by spotipy
 os.environ["SPOTIPY_CLIENT_ID"] = api_json["client_id"]
 os.environ["SPOTIPY_CLIENT_SECRET"] = api_json["client_secret"]
 os.environ["SPOTIPY_REDIRECT_URI"] = api_json["redirect_uri"]
+
+#### Get list of a users liked artists
+
+# This scope is needed to read libraries
+scope = "user-library-read"
+
+# Authenticate with the spotify API
+spotify_client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+# These variables are used to loop through the users library until the end is reached
+tracks_count = 500
+i = 0
+
+# Create an empty datafram to save the artist URIs in
+artist_uri_df = pd.DataFrame(columns = ["artist"])
+
+# Only 50 of the users tracks can be obtained at one time but the offset argument can allow the next 50 to be read.
+# If 50 tracks are requested but less than 50 comeback then the entire library has been read
+while tracks_count != 50:
+    # Request 50 tracks
+    tracks = spotify_client.current_user_saved_tracks(limit = 50, offset = i * 50)["items"]
+    # See how many tracks were returned - this will break the loop if needed
+    tracks_count = len(tracks)
+
+    # Loop through the tracks received
+    for track in tracks:
+        # For each track, get the artists - there may be more than one, x ft. y for example
+        # Getting additional artists may not be desired 
+        artist_multiple = track["track"]["artists"]
+        # Loop through the artists for a given track
+        for artist in artist_multiple:
+            # Get the uri
+            artist_single_uri = artist["uri"]
+            # Convert the uri to a data frame
+            artist_single_uri_df = pd.DataFrame([artist_single_uri], columns = ["artist"])
+            # Append the single artist to the full list
+            artist_uri_df = artist_uri_df.append(artist_single_uri_df)
+    # Increment i, this ensures the next 50 tracks are read in
+    i += 1
+
+print(artist_uri_df["artist"].unique())
 
 
 #tool_uri = 'spotify:artist:2yEwvVSSSUkcLeSTNyHKh8'
@@ -28,29 +71,6 @@ os.environ["SPOTIPY_REDIRECT_URI"] = api_json["redirect_uri"]
 #for album in albums:
 #    print(album["name"])
 #    print(album["release_date"])
-
-
-scope = "user-library-read"
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
-tracks_count = 500
-i = 0
-
-artist_uri_df = pd.DataFrame(columns = ["artist"])
-
-while tracks_count != 50:
-    tracks = sp.current_user_saved_tracks(limit = 50, offset = i * 50)["items"]
-    tracks_count = len(tracks)
-    for j in range(0, tracks_count - 1):
-        artist_multiple_uri = tracks[j]["track"]["artists"]
-        for k in range(0, len(artist_multiple_uri)):
-            artist_single_uri = [tracks[j]["track"]["artists"][k]["uri"]]
-            artist_single_uri_df = pd.DataFrame(artist_single_uri, columns = ["artist"])
-            artist_uri_df = artist_uri_df.append(artist_single_uri_df)
-    i += 1
-
-print(artist_uri_df["artist"].unique())
 
 #results = sp.current_user_saved_tracks(limit = 50, offset = 0)
 #for idx, item in enumerate(results['items']):
